33b5a11773f9c0405b130e6836ba2f09710b292c
Shane <billy.ustb2016@gmail.com>
2018-05-30 23:59:07 +0800

    first commit

---
+++ routers/router.go
@@ -0,0 +1,25 @@
+// @APIVersion 0.1.0
+// @Title social app server
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact billy.ustb2016@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"social-app-server/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
